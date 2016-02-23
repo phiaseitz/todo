5c276082c5e593b1ae6424b05531a1a9d650bfb5
phiaseitz <sophia.seitz@students.olin.edu>
2016-02-23 16:23:36 -0500

    will mongoose connection work now?

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var path = require('path');
 var index = require('./routes/index'); //all the routes
 // configuration =================
 console.log(process.env['MONGOLAB_URI']);
-mongoose.connect(String(process.env['MONGOLAB_URI']));
+mongoose.connect(process.env['MONGOLAB_URI'], ["todos"], {authMechanism: 'ScramSHA1'});
 
 var db = mongoose.connection;
 
